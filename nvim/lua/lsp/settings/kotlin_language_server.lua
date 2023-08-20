476d8fc461d9e79840ee02c11aa58d2f29517ca4
Joseph Yoon <sidnancy81@gmail.com>
2023-08-20 16:32:03 +0900

    # Changes  - Unnecessary Conifguration Removed

--- nvim/lua/lsp/settings/kotlin_language_server.lua
+++
@@ -1,9 +0,0 @@
-local jdk_home ="/opt/homebrew/Cellar/openjdk@17/17.0.8"
-return {
-  settings = {
-    cmd_env = {
-      PATH = jdk_home .. "/bin:" .. vim.env.PATH,
-      JAVA_HOME = jdk_home,
-    },
-  },
-}
