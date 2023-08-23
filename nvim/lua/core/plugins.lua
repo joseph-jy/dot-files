55b6648d2a1863693764f2023200c50f8946d344
joseph.jyoon <joseph.jyoon@kakaocorp.com>
2023-08-23 17:08:38 +0900

    - 변경사항   - colorscheme 추가   - tmux.conf.local 설정 추가

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -55,11 +55,12 @@ return packer.startup(function(use)
   use { "lewis6991/impatient.nvim", commit = "b842e16ecc1a700f62adb9802f8355b99b52a5a6" }
   use { "lukas-reineke/indent-blankline.nvim", commit = "db7cbcb40cc00fc5d6074d7569fb37197705e7f6" }
   use { "goolord/alpha-nvim", commit = "0bb6fc0646bcd1cdb4639737a1cee8d6e08bcc31" }
-	use {"folke/which-key.nvim"}
+	use { "folke/which-key.nvim" }
 
 	-- Colorschemes
   use { "folke/tokyonight.nvim", commit = "66bfc2e8f754869c7b651f3f47a2ee56ae557764" }
   use { "lunarvim/darkplus.nvim", commit = "13ef9daad28d3cf6c5e793acfc16ddbf456e1c83" }
+  use { "tiagovla/tokyodark.nvim" }
 
 	-- Cmp 
   use { "hrsh7th/nvim-cmp", commit = "b0dff0ec4f2748626aae13f011d1a47071fe9abc" } -- The completion plugin
@@ -91,6 +92,9 @@ return packer.startup(function(use)
 
 	-- Git
 	use { "lewis6991/gitsigns.nvim", commit = "2c6f96dda47e55fa07052ce2e2141e8367cbaaf2" }
+
+  -- Copilot
+  use { "github/copilot.vim" }
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
--- tmux/README.md
+++ tmux/README.md
@@ -1,10 +1,11 @@
 # tmux
 
 tmux 셋팅은 그냥 [이게](https://github.com/gpakosz/.tmux) 제일 낫다. 이걸 사용하는 게 나을 듯
+tmux.conf.local 는 Custom 하게 설정할 수 있으니 이것만 따로 보관한다.
 
 ```
 $ cd
-$ git clone https://github.com/gpakosz/.tmux.git
-$ ln -s -f .tmux/.tmux.conf
-$ cp .tmux/.tmux.conf.local .
+$ git clone https://github.com/gpakosz/.tmux.git /path/to/oh-my-tmux
+$ ln -s "/path/to/oh-my-tmux/.tmux.conf" ~/.config/tmux/tmux.conf
+$ ln -s ./tmux.conf.local ~/.config/tmux/tmux.conf.local
 ```
